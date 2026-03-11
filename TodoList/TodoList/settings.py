2e209a7f854ea8c8b52529f27b1d21dfdd019977
ml <242560587+MuLi-coder@users.noreply.github.com>
2026-03-11 10:10:36 +0800

    update2.0

--- TodoList/TodoList/settings.py
+++ TodoList/TodoList/settings.py
@@ -116,4 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = '/home/MuLi/staticfiles'
+STATIC_ROOT = '/home/MuLi/Todolist/static'
+
+ALLOWED_HOSTS = ['MuLi.pythonanywhere.com','localhost','127.0.0.1']
